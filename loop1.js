875fb65128ee332874d9e2767e03b54757889136
Raman kumar <ramankumarlal@outlook.com>
2023-02-19 20:11:11 +0530

    loop

---
+++ loop1.js
@@ -0,0 +1,23 @@
+// for (i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+const myStates = ["Rajasthan", 
+"Delhi", 
+"Bihar", 
+2120, 
+"Tamil", 
+"Maharashtra"];
+// console.log(myStates);
+// console.log(myStates.length);
+// console.log(myStates[1]);
+for (j = 0; j < myStates.length; j++){
+if (typeof myStates[j] === "string") break
+    console.log(myStates[j]);
+};
+// if i don't want to print number only string we r using if conditionin above
+// typeof determines typeof a particular value
+// if (typeof myStates[j] !== 'string') continue
+// if (typeof myStates[j] !== 'string') continue
+// if (typeof myStates[j] !== 'string') break
+
---
+++ whileloop.js
@@ -0,0 +1,21 @@
+const myStates = ["Rajasthan", 
+"Delhi", 
+"Bihar", 
+2120, 
+"Tamil", 
+"Maharashtra"];
+while loop
+let i = 0;
+
+while (i < myStates.length) {
+     i++;
+    console.log(myStates[i]);
+}
+
+// do while loop
+let i = 20;
+
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
