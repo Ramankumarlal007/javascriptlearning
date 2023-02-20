29cf820061c614cba8762052cc10e88a6258b4d8
Raman kumar <ramankumarlal@outlook.com>
2023-02-20 20:15:46 +0530

    counter learned

--- this.js
+++ this.js
@@ -1,1 +1,22 @@
 console.log(this);
+
+var user = {
+    firstName : "Raman",
+    courseCount : 32,
+    getCousreCount: function (){
+        console.log("Line 7", this);
+        function sayHello () {
+            console.log(`Rishabh is a cute little baby`);
+            console.log("lINE 9", this);
+        }
+        sayHello();
+    },
+};
+// running the below  method . 
+// below method id not a regular function call , it is through an object
+user.getCousreCount()
+// tHIS IS REGUALR FUNCTION
+function sayHello (){
+    console.log("hELLO");
+}
+sayHello()
--- whileloop.js
+++ whileloop.js
@@ -1,21 +1,21 @@
-const myStates = ["Rajasthan", 
-"Delhi", 
-"Bihar", 
-2120, 
-"Tamil", 
-"Maharashtra"];
-while loop
-let i = 0;
+// const myStates = ["Rajasthan", 
+// "Delhi", 
+// "Bihar", 
+// 2120, 
+// "Tamil", 
+// "Maharashtra"];
+// while loop
+// let i = 0;
 
-while (i < myStates.length) {
-     i++;
-    console.log(myStates[i]);
-}
+// while (i < myStates.length) {
+//      i++;
+//     console.log(myStates[i]);
+// }
 
-// do while loop
-let i = 20;
+// // do while loop
+// let i = 20;
 
-do {
-    console.log(i);
-    i++;
-} while (i < 10);
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
