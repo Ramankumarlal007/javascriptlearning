5f3e17cd68238ba64277e151bb2ddc182f66e3de
Raman kumar <ramankumarlal@outlook.com>
2023-02-23 20:24:10 +0530

    eventlistener

---
+++ DOM/ineuron/object.js
@@ -0,0 +1,19 @@
+const object = {
+    rocket : "jack",
+    home : "dullahpur",
+}
+// console.log(object);
+// console.log(object.rocket);
+// for (raman in object){console.log(raman);}
+// for (raman in object)
+// {console.log(raman.object);}
+    
+object.rocket = "daniel";
+object['year'] = 1998
+console.log(object);
+
+const obj2 = new Object()  //constructor
+obj2.redbook = "book"
+obj2.myvalue = "value book"
+
+console.log(obj2);
