301f796e4488cbad09c28f622285aba6050ffa07
Raman kumar <ramankumarlal@outlook.com>
2023-05-03 02:52:21 +0000

    js practise

--- DOM/ineuron/30April2023.js
+++ DOM/ineuron/30April2023.js
@@ -1,10 +1,30 @@
 let num1 = 7;
 let num2 = 5;
+let num3 = 2;
 
-let result = num2 > num1;
 
-if (result) {
-    console.log(`rishabh is a good boy`);
-} else {
-   console.log(`orange is green in color`); 
+// let result = num2 > num1;
+
+// if (!result) {
+//     console.log(`rishabh is a good boy`);
+//     console.log(`rishabh is a good girl`);
+// }  
+
+
+// console.log(!result);
+
+if (
+    4
+) {
+    
+} else if () {
+    
+} {
+    
 }
+
+if (condition) {
+    
+}
+
+e
